fa56b117930e5abd5248727e274509f40e66bc45
Roman Kavyrshin <rom.kawirshin@yandex.ru>
2024-04-03 14:20:31 +0300

    Переписан код с ипользованием UserDao

--- src/main/java/io/hexlet/Application.java
+++ src/main/java/io/hexlet/Application.java
@@ -1,60 +1,64 @@
 package io.hexlet;
 
-import java.math.BigDecimal;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 
-// Базовая работа с JDBC
 public class Application {
-    // Нужно указывать базовое исключение,
-    // потому что выполнение запросов может привести к исключениям
     public static void main(String[] args) throws SQLException {
-        // Создаем соединение с базой в памяти
-        // База создается прямо во время выполнения этой строчки
-        // Здесь hexlet_test — это имя базы данных
         var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test");
 
         var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
-        // Чтобы выполнить запрос, создадим объект statement
         try(var statement = conn.createStatement()) {
             statement.execute(sql);
         }
 
-        var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-        try(var statement2 = conn.createStatement()) {
-            statement2.executeUpdate(sql2);
-        }
+        var userDao = new UserDAO(conn);
+        var tommy = new User("Tommy", "1111111");
+        userDao.save(tommy);
+        userDao.save(new User("Viktor", "+71111111"));
+        userDao.save(new User("Ivan", "+7222222"));
+        userDao.save(new User("Василий", "+73333322"));
 
-        var sqlInsertData2 = "INSERT INTO users (username, phone) VALUES (?, ?);";
-        try (var preparedStatement = conn.prepareStatement(sqlInsertData2)) {
-            preparedStatement.setString(1, "Ivan");
-            preparedStatement.setString(2, "+7111111");
-            preparedStatement.executeUpdate();
+        printUsers(userDao.getAllUsers());
 
-            preparedStatement.setString(1, "Viktor");
-            preparedStatement.setString(2, "+7222222");
-            preparedStatement.executeUpdate();
-        }
+        var newUser = new User("Геннадий", "+7555544");
+        userDao.save(newUser);
+        System.out.println("new user id = " + newUser.getId());
 
-        var sqlDeleteUserByNameSql = "DELETE FROM users WHERE username = ?;";
-        try (var preparedStatement = conn.prepareStatement(sqlDeleteUserByNameSql)) {
-            preparedStatement.setString(1, "Viktor");
-            preparedStatement.executeUpdate();
-        }
+        tommy.setPhone(tommy.getPhone() + "322");
+        userDao.save(tommy);
 
-        var sqlSelectAll = "SELECT * FROM users";
-        try(var statement3 = conn.createStatement()) {
-            // Здесь вы видите указатель на набор данных в памяти СУБД
-            var resultSet = statement3.executeQuery(sqlSelectAll);
-            // Набор данных — это итератор
-            // Мы перемещаемся по нему с помощью next() и каждый раз получаем новые значения
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("username") + " " + resultSet.getString("phone"));
+        printUsers(userDao.getAllUsers());
+
+        var foundUser = userDao.find(2L);
+        foundUser.ifPresent(Application::printUser);
+
+        foundUser.ifPresent((it) -> {
+            try {
+                System.out.println("delete by id " + it.getId() + " = " + userDao.deleteById(it.getId()));
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
-        }
+        });
 
-        // Закрываем соединение
+        printUsers(userDao.getAllUsers());
+
+        System.out.println("delete by username " + "Василий" + " = " + userDao.deleteByUsername("Василий"));
+
+        printUsers(userDao.getAllUsers());
+
         conn.close();
+    }
+
+    public static void printUser(User user) {
+        printUsers(List.of(user));
+    }
+
+    public static void printUsers(List<User> users) {
+        System.out.println("-----------");
+        for (User user: users) {
+            System.out.println(user.getId() + " " + user.getUsername() + " " + user.getPhone());
+        }
     }
 }
